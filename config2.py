b70f346268f19f654f96a5a076f2fb1e28c4edf0
Tracy Ammann <tea1986@gmail.com>
2021-07-19 22:17:49 -0700

    updated app.py and config2 files

---
+++ config2.py
@@ -0,0 +1,4 @@
+# username = <Add PostgreSQL Username Here>
+username = "waljilkidydguz"
+# password = <Add PostgreSQL Password Here>
+password = "689b06ad421784ad4ea17abfa4d649ffe291a27c691edc52169cf33b166ffc3e"
