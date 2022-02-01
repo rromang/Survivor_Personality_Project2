1febb30fac2ed80a7bfa963929e70902da21a3cb
Rosa Roman <rrgomez2008@gmail.com>
2022-01-31 17:56:47 -0700

    Update config2.py

--- config2.py
+++ config2.py
@@ -1,4 +1,4 @@
 # username = <Add PostgreSQL Username Here>
-username = "waljilkidydguz"
+username = ""
 # password = <Add PostgreSQL Password Here>
-password = "689b06ad421784ad4ea17abfa4d649ffe291a27c691edc52169cf33b166ffc3e"
+password = ""
